c3836c07aab6797153142698327b2a464ef9cb43
Zhuohu Guo <zhuohu.guo@mdpi.com>
2025-08-06 14:17:46 +0800

    优化内存参数

--- conftest.py
+++ conftest.py
@@ -51,6 +51,7 @@ def login():
     options.add_argument('--disable-notifications')
     options.add_argument('--disable-renderer-backgrounding')
     options.add_argument('--disable-sync')
+
     options.add_argument("--disable-blink-features=AutomationControlled")
     options.add_experimental_option("excludeSwitches", ["enable-automation"])
     options.add_experimental_option("useAutomationExtension", False)
